71c594e0a9217769f187632c93df876570a997bd
justynya <dojustynyhobot@gmail.com>
2016-06-06 22:53:40 +0200

    Update gaborjerome.R

--- gaborjerome.R
+++ gaborjerome.R
@@ -271,10 +271,10 @@ run.trials(trial.code, condition = cnd, expand.grid(side = c('left', 'right'), s
 
 docx.instr('Instrukcja3.docx')
 
-## Etap właściwy, 96 prób na każdy czas ze skalą, różne czasy
+## Etap właściwy, 80 prób na każdy czas ze skalą, różne czasy
 run.trials(trial.code, condition = cnd, expand.grid(side = c('left', 'right'), scale = cnd, stage = 'test',
                                                     withscale = 1, feedback = 0, duration = c(16, 32, 64, 128)),
-                                        b = 48, record.session = T)
+                                        b = 40, record.session = T)
 
 docx.instr('Instrukcja4.docx', F)
 
