cb4e5338c24ee626f96284da6fee7fb44fcfd948
justynya <dojustynyhobot@gmail.com>
2016-07-13 10:45:49 +0200

    Update gaborjerome.R

--- gaborjerome.R
+++ gaborjerome.R
@@ -244,7 +244,7 @@ docx.instr = function(file, ask.if.done = T){
 
 TASK.NAME <<- 'gaborjerome'
 
-cnd = db.random.condition(c('ias'))
+cnd = db.random.condition(c('ias, cs, pas, cpas'))
 
 docx.instr(c(pas = 'InstrukcjaPAS.docx', cpas = 'InstrukcjaCPAS.docx', ias = 'InstrukcjaIAS.docx', cs = 'InstrukcjaCS.docx')[cnd])
 
